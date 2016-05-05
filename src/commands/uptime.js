0de8e18646ebbb037849e8af9588b7df45d10073
Ian Mitchell <ianm@zillow.com>
2016-05-05 10:53:02 -0700

    Added Uptime Formatting

--- src/commands/uptime.js
+++ src/commands/uptime.js
@@ -1,12 +1,15 @@
 const debug = require('debug');
 const client = require('../client');
+const moment = require('moment');
+
 
 const log = debug('Uptime');
 
 exports.messageTriggered = message => message === 'uptime';
 exports.message = () => {
   log(`Uptime called: ${client.uptime}`);
-  return client.uptime;
+  const uptime = moment(Date.now() - client.uptime).fromNow(true);
+  return `Aquarius has been up for ${uptime}`;
 };
 
 exports.helpTriggered = (message) => message.includes('uptime');
