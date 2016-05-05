cc2b57f8547806e829769d1a25c6e2b72e992e07
Elmer Urbano <elmerurbano@gmail.com>
2016-05-04 22:06:38 -0700

    added replies

--- src/commands/reply.js
+++ src/commands/reply.js
@@ -3,11 +3,13 @@ const log = debug('Reply');
 
 const responses = new Map();
 responses.set('ping', 'pong');
+responses.set('sing', 'song');
 responses.set('bot respond', "I'm a pretty stupid bot.");
 responses.set('bot be nice', 'sorry :(');
 responses.set('gj bot', 'thx');
 responses.set('thx bot', 'np');
 responses.set('bot pls', '( ¬‿¬)');
+responses.set('(╯°□°)╯︵ ┻━┻', '┬─┬﻿ ノ( ゜-゜ノ)');
 
 exports.messageTriggered = message => responses.has(message.toLowerCase());
 exports.message = message => {
