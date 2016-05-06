04e6ef817bc8daf0ddedaa6523a61ea07b675679
Elmer Urbano <elmerurbano@gmail.com>
2016-05-06 14:21:04 -0700

    reply.js - added a few more replies

--- src/commands/reply.js
+++ src/commands/reply.js
@@ -4,11 +4,14 @@ const log = debug('Reply');
 const responses = new Map();
 responses.set('ping', 'pong');
 responses.set('sing', 'song');
+responses.set('hi bot', 'hey beautiful c;');
 responses.set('bot respond', "I'm a pretty stupid bot.");
-responses.set('bot be nice', 'sorry :(');
+responses.set('bot be nice', 'sorry :c');
 responses.set('gj bot', 'thx');
 responses.set('thx bot', 'np');
 responses.set('bot pls', '( ¬‿¬)');
+responses.set('not now bot', 'aww ok :c');
+responses.set('go away bot', 'I just wanted to be friends');
 responses.set('(╯°□°)╯︵ ┻━┻', '┬─┬﻿ ノ( ゜-゜ノ)');
 responses.set( '┬─┬﻿ ノ( ゜-゜ノ)', 'THAT\'S MY JOB');
 
