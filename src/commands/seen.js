dc0290347d6300e49a05ff5d94beafae9ba1e3bb
Ian Mitchell <ianm@zillow.com>
2016-05-09 16:27:29 -0700

    Adds a Seen Command
    Fixes #21

---
+++ src/commands/seen.js
@@ -0,0 +1,59 @@
+const debug = require('debug');
+const moment = require('moment');
+const config = require('../../config');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.development.url);
+const Seen = sequelize.import('../models/seen');
+
+const log = debug('Seen');
+
+const message = msg => {
+  const seenRegex = /^@[#\w]+ seen @[#\w]+/i;
+  const seenMatch = msg.cleanContent.match(seenRegex);
+
+  if (seenMatch) {
+    const user = msg.mentions[1];
+
+    // untagged @mention, which Regex returns as a false positive
+    if (user === undefined) {
+      return false;
+    }
+
+    log(`Seen request for ${user}`);
+
+    if (msg.client.users.get('id', user.id).status !== 'offline') {
+      return "They're online right now!";
+    }
+
+    Seen.findOrCreate({
+      where: {
+        userId: user.id,
+      },
+      defaults: {
+        lastSeen: 0,
+      },
+    }).spread((seen, created) => {
+      let time = seen.lastSeen;
+
+      if (created || seen.lastSeen === 0) {
+        msg.client.sendMessage(msg.channel, `I don't have a record for ${user}`);
+        return;
+      }
+
+      time = moment(seen.lastSeen * 1000);
+
+      msg.client.sendMessage(msg.channel, `${user} last seen ${time.fromNow()}`);
+      return;
+    });
+
+    return false;
+  }
+
+  return false;
+};
+
+module.exports = {
+  name: 'seen',
+  help: '`@bot seen @user`. Displays the time since last user was online.',
+  message,
+};
