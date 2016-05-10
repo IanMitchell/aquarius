91cddae467623e41a1dd569a77e14a8e252af396
Elmer Urbano <elmerurbano@gmail.com>
2016-05-09 21:14:02 -0700

    moved dickbutt.js into reply.js

--- src/commands/reply.js
+++ src/commands/reply.js
@@ -1,6 +1,8 @@
 const debug = require('debug');
 const log = debug('Reply');
 
+const dickbutt_img = 'https://i.imgur.com/etjgJ2D.jpg';
+
 const greetings = [
   'hi',
   'sup',
@@ -28,7 +30,8 @@ responses.set('not now bot', 'aww ok :c');
 responses.set('go away bot', 'I have nowhere else to go');
 responses.set('not cool bot', 'I just wanted to be friends');
 responses.set('(╯°□°）╯︵ ┻━┻', '┬─┬﻿ ノ( ゜-゜ノ)');
-responses.set( '┬─┬﻿ ノ( ゜-゜ノ)', 'THAT\'S MY JOB');
+responses.set( '┬─┬﻿ ノ( ゜-゜ノ)', "THAT'S MY JOB");
+responses.set('dickbutt', dickbutt_img);
 
 const message = msg => {
   if (responses.has(msg.cleanContent.toLowerCase())) {
