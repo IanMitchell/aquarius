49d2266fa24556d34fd0a051d77eb777b26b795e
Ian Mitchell <ianm@zillow.com>
2016-05-27 13:27:09 -0700

    Fixes .env Check
    Oops?

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const debug = require('debug');
 const log = debug('Boot');
 
 // Hacky check for dev/production startup
-const file = fs.statSync('.env');
+try {
+  fs.statSync('.env');
 
-if (file === null || file.code === 'ENOENT') {
-  log('Production environment, reading from environmental variables');
-} else {
   log('Development environment, reading from `.env`');
   require('dotenv').config();
+} catch (e) {
+  log('Production environment, reading from environmental variables');
 }
 
 // Start the bot
