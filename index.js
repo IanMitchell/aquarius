e63467ff82c798f28009a313035c835f4c82aaad
Ian Mitchell <ianm@zillow.com>
2016-05-27 13:24:59 -0700

    Boot environment fix
    Fixes #32

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // This is nasty, but prevents requiring different commands
 // for windows machines
-require('dotenv').config();
-
 process.env.DEBUG = '*,-ref,-ref:*,-superagent';
+
+const fs = require('fs');
+const debug = require('debug');
+const log = debug('Boot');
+
+// Hacky check for dev/production startup
+const file = fs.statSync('.env');
+
+if (file === null || file.code === 'ENOENT') {
+  log('Production environment, reading from environmental variables');
+} else {
+  log('Development environment, reading from `.env`');
+  require('dotenv').config();
+}
 
 // Start the bot
 require('./src/aquarius');
