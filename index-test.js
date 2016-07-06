263be0692111c39151670c4799e6a8cf993f79d0
Ian Mitchell <ianm@zillow.com>
2016-07-06 13:47:42 -0700

    Fixes CI Builds

--- index-test.js
+++ index-test.js
@@ -1,4 +1,13 @@
 // This is nasty, but prevents requiring different commands
 // for windows machines
 process.env.DEBUG = '-*';
-require('dotenv').config({ path: `${__dirname}/.env` });
+
+// Hacky check for dev/CI startup
+const fs = require('fs');
+
+try {
+  fs.statSync(`${__dirname}/.env`);
+  require('dotenv').config({ path: `${__dirname}/.env` });
+} catch (e) {
+  console.log('CI Build, reading from environment.');
+}
