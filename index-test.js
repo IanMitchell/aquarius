ba537cedb819643b10d23ef8311286d2a9b3e99b
Ian Mitchell <ianm@zillow.com>
2016-07-11 16:23:33 -0700

    Clean up Test Output

--- index-test.js
+++ index-test.js
@@ -1,10 +1,13 @@
+const fs = require('fs');
+
 // This is nasty, but prevents requiring different commands
 // for windows machines
 process.env.DEBUG = '-*';
 
 // Account for CI environments
 try {
+  fs.statSync(`${__dirname}/.env`);
   require('dotenv').config({ path: `${__dirname}/.env` });
 } catch (e) {
-  // Read from environmental variables
+  // CI Build
 }
