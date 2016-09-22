8d4f00f279f733e288e32ddfd18b2794efddec44
Ian Mitchell <ian.mitchel1@live.com>
2016-09-21 21:09:10 -0700

    Adds Admin Commands
    Oops...

--- src/aquarius/index.js
+++ src/aquarius/index.js
@@ -1,4 +1,5 @@
 // Aquarius Core Framework
+const Admin = require('./admin/admin');
 const Client = require('./core/client');
 const Command = require('./core/command');
 const Triggers = require('./core/triggers');
@@ -15,6 +16,7 @@ require('./prototypes/string');
 require('./prototypes/array');
 
 module.exports = {
+  Admin,
   Client,
   Command,
   Triggers,
