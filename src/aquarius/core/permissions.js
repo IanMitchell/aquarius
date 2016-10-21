f5c67578e4ee0f7e773ba459fd24734726f54094
Ian Mitchell <ianm@zillowgroup.com>
2016-10-21 12:43:54 -0700

    Command Check Fix

--- src/aquarius/core/permissions.js
+++ src/aquarius/core/permissions.js
@@ -39,7 +39,7 @@ function isGuildMuted(guild, user) {
 }
 
 function isCommandEnabled(guild, command) {
-  return settings.getPermission(guild.id, command.constructor.name) !== false;
+  return settings.getCommands(guild.id).includes(command.constructor.name);
 }
 
 function hasPermission(guild, user, command) {
