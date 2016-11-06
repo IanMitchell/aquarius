27ca03ed972f55b73f321f00bbc08ebfe8ef5e24
Ian Mitchell <ianm@zillowgroup.com>
2016-11-06 08:50:39 -0800

    Adds hashtag support
    This is for you skiddiks

---
+++ src/commands/hashtag.js
@@ -0,0 +1,25 @@
+const Aquarius = require('../aquarius');
+
+class Hashtag extends Aquarius.Command {
+  constructor() {
+    super();
+    this.description = 'Adds hashtags using emoji responses.';
+  }
+
+  message(msg) {
+    if (msg.cleanContent.includes('hashtag')) {
+      this.log('Adding Hashtag Reaction');
+
+      msg.addReaction('✋')
+        // .then(() => msg.addReaction('#'))
+        .then(() => msg.addReaction(encodeURI('🇭')))
+        .then(() => msg.addReaction(encodeURI('🇹')))
+        .then(() => msg.addReaction(encodeURI('🇦')))
+        .then(() => msg.addReaction(encodeURI('🇬')))
+        .then(() => msg.addReaction(encodeURI('🇸')))
+        .catch(this.log);
+    }
+  }
+}
+
+module.exports = new Hashtag();
