20ae70ee33dcdecc872517680ae09d45a3a2c9dc
bgladx64 <bgladx64@gmail.com>
2016-11-12 19:18:37 -0800

    Adds Hearthstone Command

---
+++ src/commands/hearthstone.js
@@ -0,0 +1,40 @@
+const Aquarius = require('../aquarius');
+const hdb = require('hearthstone-db');
+
+class Hearthstone extends Aquarius.Command {
+  constructor() {
+    super();
+    this.description = 'Displays Hearthstone Card Image';
+  }
+
+  helpMessage() {
+    let msg = super.helpMessage();
+
+    msg += 'Usage:\n';
+    msg += '```[[Tirion Fordring]]```';
+    return msg;
+  }
+
+  message(msg) {
+    const hearthInput = Aquarius.Triggers.cardTrigger(msg);
+
+    if (hearthInput) {
+      hearthInput.forEach(cardInput => {
+        this.log(`Request for ${cardInput[1]}`);
+        const cardName = cardInput[1].toLowerCase();
+
+        Aquarius.Loading.startLoading(msg.channel);
+        hdb.allCards.some(card => {
+          if (card.name.toLowerCase() === cardName) {
+            msg.channel.sendFile(card.image_url, `${card.name}.png`);
+            return true;
+          }
+          return false;
+        });
+        Aquarius.Loading.stopLoading(msg.channel);
+      });
+    }
+  }
+}
+
+module.exports = new Hearthstone();
