39caaf4e00ad1e6e7991b1e2693432266eb1e9e1
Zane Neuschuler <neuschuler@gmail.com>
2019-05-15 19:16:13 -0700

    actually ship it now

--- src/bot/commands/stocks.js
+++ src/bot/commands/stocks.js
@@ -1,28 +1,88 @@
 import debug from 'debug';
 import { RichEmbed, Permissions } from 'discord.js';
 import fetch from 'node-fetch';
+import { fromUnixTime, format } from 'date-fns';
 
 const log = debug('stocks');
 
 // TODO: Write Info
 export const info = {
   name: 'stocks',
-  description: '',
+  description: 'Get some handy dandy stock info for your day trading!',
   permissions: [Permissions.FLAGS.EMBED_LINKS],
-  usage: '',
+  usage: '```@Aquarius stocks <stock symbol>```',
 };
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const API = 'https://cloud.iexapis.com/stable/stock/';
 
 const TOKEN = `quote?token=${process.env.IEXCLOUD_KEY}`;
 
-function Unix_timestamp(t) {
-  const dt = new Date(t * 1000);
-  const hr = dt.getHours();
-  const m = `0${dt.getMinutes()}`;
-  const s = `0${dt.getSeconds()}`;
-  return `${hr}:${m.substr(-2)}:${s.substr(-2)}`;
+function createStocksEmbed(data) {
+  const change = `Change ${
+    data.change >= 0
+      ? ':chart_with_upwards_trend:'
+      : ':chart_with_downwards_trend:'
+  }`;
+  const embed = new RichEmbed({
+    description: `*Stock Symbol: $${data.symbol}*`,
+    color: 16318579,
+    timestamp: `${fromUnixTime(data.latestUpdate / 1000)}`,
+    footer: {
+      text: 'Data provided by IEX Cloud',
+    },
+    author: {
+      name: `${data.companyName} (Click here for more info!)`,
+      url: `https://finance.yahoo.com/quote/${data.symbol}/`,
+    },
+    fields: [
+      {
+        name: 'Open :bell:',
+        value: `${data.open}`,
+        inline: true,
+      },
+      {
+        name: 'Closing :no_bell:',
+        value: `${data.close}`,
+        inline: true,
+      },
+      {
+        name: 'High :dollar:',
+        value: `${data.high}`,
+        inline: true,
+      },
+      {
+        name: 'Low :fire:',
+        value: `${data.low}`,
+        inline: true,
+      },
+      {
+        name: `${change}`,
+        value: `${data.change}`,
+        inline: true,
+      },
+      {
+        name: '52 Week High  :money_with_wings:',
+        value: `${data.week52High}`,
+        inline: true,
+      },
+      {
+        name: '52 Week Low :scream:',
+        value: `${data.week52Low}`,
+        inline: true,
+      },
+      {
+        name: 'Market Cap :moneybag:',
+        value: `$${numberWithCommas(data.marketCap)}`,
+        inline: true,
+      },
+    ],
+  });
+  return embed;
 }
+
 /** @type {import('../../typedefs').Command} */
 export default async ({ aquarius, analytics }) => {
   aquarius.onCommand(/^stocks (?<sign>.+)$/i, async (message, { groups }) => {
@@ -40,20 +100,18 @@ export default async ({ aquarius, analytics }) => {
     }
 
     try {
+      aquarius.loading.start(message.channel);
       const response = await fetch(`${API}/${groups.sign}/${TOKEN}`);
       const data = await response.json();
-      message.channel.send(`Stock info for ${data.companyName} (${
-        data.symbol
-      }) at ${Unix_timestamp(data.latestUpdate)}\n
-      Today's high: ${data.high}\n
-      Today's low: ${data.low}\n
-      Today's open: ${data.open}\n
-      Today's closing: ${data.close}`);
+      const embed = await createStocksEmbed(data);
+      message.channel.send(embed);
+      aquarius.loading.stop(message.channel);
     } catch (error) {
       log(error);
       message.channel.send(
-        'Sorry, something went wrong! (Maybe you mistyped the symbol?'
+        'Sorry, something went wrong! (Maybe you mistyped the symbol?)'
       );
+      aquarius.loading.stop(message.channel);
     }
     /**
      * Chart
