0ccbb3a138eff4ab5200d841ec2f4cfb899de13f
Ian Mitchell <ian.mitchel1@live.com>
2019-11-22 15:29:30 -0800

    Annotated Failure Test

--- src/lib/helpers/__test__/regex.test.js
+++ src/lib/helpers/__test__/regex.test.js
@@ -21,75 +21,133 @@ const MESSAGES = {
   mentionUserNickname: 'Mention <@!356528540742582282> User Nickname',
   mentionUserId: 'Mention <@356528540742582282> User ID',
   mentionUser: 'Mention <@103635479097769984> User No Nickname',
-  mention: '',
 };
 
-function filterMessages(name) {
-  return Object.keys(MESSAGES)
-    .filter(key => key !== name)
-    .map(key => MESSAGES[key]);
+function getAllMessagesBut(message) {
+  return Object.values(MESSAGES).filter(msg => msg !== message);
 }
 
 describe('MENTION', () => {
-  test.todo('Captures ID');
-
-  test.todo('Matches User');
-
-  test.todo('Matches User ID');
-
-  test.todo('Matches User with Nick');
-
-  test.todo('Matches Channel');
-
-  test.todo('Matches Role');
+  test.each([
+    ['Animated Emoji', MESSAGES.animatedEmoji, '588234282468769814'],
+    ['Custom Emoji', MESSAGES.customEmoji, '340934564807573505'],
+    ['Role', MESSAGES.mentionRole, '481159087929688076'],
+    ['Channel', MESSAGES.mentionChannel, '356522910569201665'],
+    ['User Nickname', MESSAGES.mentionUserNickname, '356528540742582282'],
+    ['User ID', MESSAGES.mentionUserId, '356528540742582282'],
+    ['User', MESSAGES.mentionUser, '103635479097769984'],
+  ])('Captures ID for %s', (title, message, id) => {
+    const match = message.match(MENTION);
+    expect(match.groups.id).toBe(id);
+  });
 });
 
 describe('MENTION_USER', () => {
-  test.todo('Captures ID');
+  test('Captures ID', () => {
+    const match = MESSAGES.mentionUser.match(MENTION_USER);
+    expect(match.groups).toEqual({
+      id: '103635479097769984',
+    });
+  });
 
-  test.todo('Matches User');
+  test('Matches User', () => {
+    getAllMessagesBut(MESSAGES.mentionUser).forEach(message => {
+      const match = message.match(MENTION_USER);
+      expect(match).toBeNull();
+    });
+  });
 });
 
 describe('MENTION_USER_ID', () => {
-  test.todo('Captures ID');
+  test('Captures ID', () => {
+    const match = MESSAGES.mentionUserId.match(MENTION_USER_ID);
+    expect(match.groups).toEqual({
+      id: '356528540742582282',
+    });
+  });
 
-  test.todo('Matches User ID');
+  test('Only Matches User ID', () => {
+    getAllMessagesBut(MESSAGES.mentionUserId).forEach(message => {
+      const match = message.match(MENTION_USER_ID);
+      expect(match).toBeNull();
+    });
+  });
 });
 
-describe('MENTION_USER_NICK', () => {
-  test.todo('Captures ID');
+describe('MENTION_USER_NICKNAME', () => {
+  test('Captures ID', () => {
+    const match = MESSAGES.mentionUserNickname.match(MENTION_USER_NICKNAME);
+    expect(match.groups).toEqual({
+      id: '356528540742582282',
+    });
+  });
 
-  test.todo('Matches User Nick');
+  test('Only Matches User Nickname', () => {
+    getAllMessagesBut(MESSAGES.mentionUserNickname).forEach(message => {
+      const match = message.match(MENTION_USER_NICKNAME);
+      expect(match).toBeNull();
+    });
+  });
 });
 
 describe('MENTION_CHANNEL', () => {
-  test.todo('Captures ID');
+  test('Captures ID', () => {
+    const match = MESSAGES.mentionChannel.match(MENTION_CHANNEL);
+    expect(match.groups).toEqual({
+      id: '356522910569201665',
+    });
+  });
 
-  test.todo('Matches Channel');
+  test('Only Matches Channel', () => {
+    getAllMessagesBut(MESSAGES.mentionChannel).forEach(message => {
+      const match = message.match(MENTION_CHANNEL);
+      expect(match).toBeNull();
+    });
+  });
 });
 
 describe('MENTION_ROLE', () => {
-  test.todo('Captures ID');
+  test('Captures ID', () => {
+    const match = MESSAGES.mentionRole.match(MENTION_ROLE);
+    expect(match.groups).toEqual({
+      id: '481159087929688076',
+    });
+  });
 
-  test.todo('Matches Role');
+  test('Only Matches Role', () => {
+    getAllMessagesBut(MESSAGES.mentionRole).forEach(message => {
+      const match = message.match(MENTION_ROLE);
+      expect(match).toBeNull();
+    });
+  });
 });
 
 describe('EMOJI', () => {
   test.todo('Captures Name and ID');
 
-  test.todo('Matches Emoji');
+  test.todo('Only Matches Emoji');
 });
 
 describe('CUSTOM_EMOJI', () => {
-  test.todo('Captures Name and ID');
+  test('Captures Name and ID', () => {
+    const match = MESSAGES.customEmoji.match(CUSTOM_EMOJI);
+    expect(match.groups).toEqual({
+      id: '340934564807573505',
+      name: 'fbslightsmile',
+    });
+  });
 
-  test.todo('Matches Custom Emoji');
+  test('Only Matches Custom Emoji', () => {
+    getAllMessagesBut(MESSAGES.customEmoji).forEach(message => {
+      const match = message.match(CUSTOM_EMOJI);
+      expect(match).toBeNull();
+    });
+  });
 });
 
 describe('ANIMATED_EMOJI', () => {
   test('Captures Name and ID', () => {
     const match = MESSAGES.animatedEmoji.match(ANIMATED_EMOJI);
-    expect(match).not.toBeNull();
     expect(match.groups).toEqual({
       id: '588234282468769814',
       name: 'eyesshaking',
@@ -97,7 +155,7 @@ describe('ANIMATED_EMOJI', () => {
   });
 
   test('Only Matches Animated Emoji', () => {
-    filterMessages('animatedEmoji').forEach(message => {
+    getAllMessagesBut(MESSAGES.animatedEmoji).forEach(message => {
       const match = message.match(ANIMATED_EMOJI);
       expect(match).toBeNull();
     });
@@ -107,7 +165,6 @@ describe('ANIMATED_EMOJI', () => {
 describe('BRACKET', () => {
   test('Captures Name', () => {
     const match = MESSAGES.bracket.match(BRACKET);
-    expect(match).not.toBeNull();
     expect(match.groups.name).toBe('Jace Beleren');
   });
 });
