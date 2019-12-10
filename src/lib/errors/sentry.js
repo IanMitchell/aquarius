f460406f1e63bd67d03d611d88c33c535b98359a
Ian Mitchell <ian.mitchel1@live.com>
2019-12-10 13:40:22 -0800

    Fix Export

--- src/lib/errors/sentry.js
+++ src/lib/errors/sentry.js
@@ -2,10 +2,8 @@ import * as Sentry from '@sentry/node';
 
 export default (() => {
   if (process.env.NODE_ENV === 'production') {
-    return Sentry.init({ dsn: process.env.SENTRY });
+    Sentry.init({ dsn: process.env.SENTRY });
   }
 
-  return {
-    captureException: () => {},
-  };
+  return Sentry;
 })();
