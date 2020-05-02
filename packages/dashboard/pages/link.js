eaa34092dfa286ea175dc257d21e5f8e999093ea
Ian Mitchell <ian.mitchel1@live.com>
2020-05-01 17:54:55 -0700

    Temporarily Hardcode Link

--- packages/dashboard/pages/link.js
+++ packages/dashboard/pages/link.js
@@ -1,15 +1,19 @@
 /* global window */
 import { useEffect } from 'react';
-import useSWR from 'swr';
 
 export default function Link() {
-  const { data } = useSWR('https://api.aquarius.sh/link');
+  // const { data } = useSWR('https://api.aquarius.sh/link');
+
+  // useEffect(() => {
+  //   if (data?.url) {
+  //     window.location = data.url;
+  //   }
+  // }, [data]);
 
   useEffect(() => {
-    if (data?.url) {
-      window.location = data.url;
-    }
-  }, [data]);
+    window.location =
+      'https://discordapp.com/oauth2/authorize/?client_id=176793254350684160&scope=bot&permissions=1543892032';
+  }, []);
 
   return null;
 }
