f973da67563c1777d5b44f83f52b0f3d6d3ad02b
Ian Mitchell <ian.mitchel1@live.com>
2020-05-03 15:46:15 -0700

    Add Morse Command (#526)

---
+++ packages/aquarius/src/bot/commands/morse.js
@@ -0,0 +1,39 @@
+import debug from 'debug';
+import dedent from 'dedent-js';
+import morse from 'morse';
+
+const log = debug('DadJoke');
+
+export const info = {
+  name: 'morse',
+  description: 'Encode or decode morse code.',
+  usage: dedent`
+  To encode a message:
+  \`\`\`@Aquarius morse encode <message>\`\`\`
+  To decode a message:
+  \`\`\`@Aquarius morse decode <message>\`\`\`
+  `,
+};
+
+/** @type {import('../../typedefs').Command} */
+export default async ({ aquarius, analytics }) => {
+  aquarius.onCommand(
+    /^morse encode (?<input>.+)$/i,
+    async (message, { groups }) => {
+      log(`Encoding "${groups.input}"`);
+      const str = morse.encode(groups.input);
+      message.channel.send(str);
+      analytics.trackUsage('enocde', message);
+    }
+  );
+
+  aquarius.onCommand(
+    /^morse decode (?<input>.+)$/i,
+    async (message, { groups }) => {
+      log(`Decoding "${groups.input}"`);
+      const str = morse.decode(groups.input);
+      message.channel.send(str);
+      analytics.trackUsage('decode', message);
+    }
+  );
+};
