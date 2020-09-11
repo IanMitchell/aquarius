ac324a8cd4f65bcfc43f02ab477c2b1c717098b7
Ian Mitchell <ian.mitchell@hey.com>
2020-09-11 10:23:48 -0700

    Switch to IQ Air
    Unfortunately we lose the forecast

--- packages/aquarius/src/bot/commands/aqi.js
+++ packages/aquarius/src/bot/commands/aqi.js
@@ -4,7 +4,6 @@ import { MessageEmbed, Permissions } from 'discord.js';
 import NodeCache from 'node-cache';
 import fetch from 'node-fetch';
 import { getEmbedColorFromHex } from '../../core/helpers/colors';
-import { getStandardDate } from '../../core/helpers/dates';
 import { ONE_HOUR } from '../../core/helpers/times';
 
 const log = debug('AQI');
@@ -19,20 +18,35 @@ export const info = {
 
 // Defined here:
 // https://docs.airnowapi.org/aq101
-const COLORS = {
-  '1': getEmbedColorFromHex('#00e400'),
-  '2': getEmbedColorFromHex('#ffff00'),
-  '3': getEmbedColorFromHex('#ff7e00'),
-  '4': getEmbedColorFromHex('#ff0000'),
-  '5': getEmbedColorFromHex('#8f3f97'),
-  '6': getEmbedColorFromHex('#7e0023'),
-};
+function getColorCode(aqi) {
+  if (aqi <= 50) {
+    return getEmbedColorFromHex('#00e400');
+  }
+
+  if (aqi <= 100) {
+    return getEmbedColorFromHex('#ffff00');
+  }
+
+  if (aqi <= 150) {
+    return getEmbedColorFromHex('#ff7e00');
+  }
+
+  if (aqi <= 200) {
+    return getEmbedColorFromHex('#ff0000');
+  }
+
+  if (aqi <= 300) {
+    return getEmbedColorFromHex('#8f3f97');
+  }
+
+  return getEmbedColorFromHex('#7e0023');
+}
 
 async function getLatitudeAndLongitude(searchTerm) {
   const response = await fetch(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
       searchTerm
-    )}.json?access_token=${process.env.MAPBOX_API_KEY}&country=US`
+    )}.json?access_token=${process.env.MAPBOX_API_KEY}`
   );
   const data = await response.json();
 
@@ -43,35 +57,10 @@ async function getLatitudeAndLongitude(searchTerm) {
 }
 
 async function getForecast(longitude, latitude) {
-  const date = new Date();
-  const dateStr = `${date.getFullYear()}-${(date.getMonth() + 1)
-    .toString()
-    .padStart(2, '0')}-${date.getDate()}`;
-
   const response = await fetch(
-    `http://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=${latitude}&longitude=${longitude}&date=${dateStr}&distance=25&API_KEY=${process.env.AIR_NOW_API_KEY}`
+    `http://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=${process.env.IQAIR_API_KEY}`
   );
-  const data = await response.json();
-
-  return data.filter((entry) => entry.ParameterName === 'PM2.5');
-}
-
-function formatAQI(day) {
-  const { ActionDay, AQI, Category } = day;
-
-  let str = '';
-
-  if (ActionDay) {
-    str += '⚠️ ';
-  }
-
-  if (AQI && AQI !== -1) {
-    str += `${AQI}: `;
-  }
-
-  str += Category?.Name;
-
-  return str;
+  return response.json();
 }
 
 /** @type {import('../../typedefs').Command} */
@@ -93,34 +82,30 @@ export default async ({ aquarius, analytics }) => {
           );
           const forecast = await getForecast(...coordinates);
 
-          if (!forecast || forecast.length === 0) {
+          if (forecast?.status !== 'success') {
             log(`Unable to find information for ${groups.location}`);
             message.channel.send(
-              "Sorry, I wasn't able to find information about that location. This command is currently limited to certain locations within the United States."
+              "Sorry, I wasn't able to find information about that location."
             );
             return;
           }
 
-          cache.set(groups.location, forecast);
-          data = forecast;
+          cache.set(groups.location, forecast.data);
+          data = forecast.data;
         }
-
-        const [today, ...futureDays] = data;
 
         const embed = new MessageEmbed()
           .setTitle(
-            `PM2.5 Air Quality Index for ${today?.ReportingArea}, ${today?.StateCode}`
+            `PM2.5 Air Quality Index for ${data.city}, ${data.state} ${data.country}`
           )
-          .setDescription(`The current AQI is **${formatAQI(today)}**`)
-          .setColor(COLORS[today?.Category?.Number])
-          .setFooter('Information from AirNow');
-
-        futureDays.forEach((day) =>
-          embed.addField(
-            getStandardDate(new Date(day?.DateForecast)),
-            formatAQI(day)
+          .setDescription(
+            `The current AQI is **${data.current.pollution.aqius}**`
           )
-        );
+          .setColor(getColorCode(data.current.pollution.aqius))
+          .setFooter(
+            'Information from IQ Air',
+            'https://www.iqair.com/assets/favicons/favicon-32x32.png'
+          );
 
         message.channel.send(embed);
       } catch (error) {
