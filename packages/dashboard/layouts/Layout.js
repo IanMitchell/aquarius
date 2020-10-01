2db3cff9a10e0c3350cff43069e7c86936077a27
Ian Mitchell <ian.mitchell@hey.com>
2020-10-01 16:39:27 -0700

    Fix Lint Error

--- packages/dashboard/layouts/Layout.js
+++ packages/dashboard/layouts/Layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable consistent-return */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
