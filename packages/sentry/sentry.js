eed0466a5654d1e013da04395ffbc5e8aa11f3f8
Ian Mitchell <ian.mitchell@hey.com>
2021-03-31 22:18:39 -0700

    Correctly Apply Sentry Scope
    Fixes #500

--- packages/sentry/sentry.js
+++ packages/sentry/sentry.js
@@ -16,23 +16,32 @@ export default (() => {
     ...Sentry,
 
     /**
-     * Adds context to Sentry for better error handling.
+     * Adds Message context to Sentry for better error reporting.
      * @param {Message} message - Message to configure scope with
+     * @param {Function} fn - Handler function to call within the configured scope
      */
-    configureMessageScope: (message) => {
-      Sentry.configureScope((scope) => {
+    withMessageScope: (message, fn) => {
+      Sentry.withScope((scope) => {
         const { tag, id } = message.author;
         scope.setUser({ username: tag, id });
 
         if (message.guild) {
           scope.setExtra('Guild ID', message.guild.id);
+          scope.setExtra('Guild Name', message.guild.name);
           scope.setExtra('Channel ID', message.channel.id);
+          scope.setExtra('Channel Name', message.channel.name);
         } else {
           scope.setExtra('Channel Type', 'DM');
         }
 
         scope.setExtra('Message', message.content);
         scope.setExtra('Message ID', message.id);
+
+        try {
+          fn();
+        } catch (error) {
+          Sentry.captureException(error);
+        }
       });
     },
   };
