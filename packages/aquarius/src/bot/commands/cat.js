7056d5f6697f55e2e270fe449d76fe1e0658137b
Tiemen <ThaTiemsz@users.noreply.github.com>
2021-07-05 02:38:48 +0200

    Fix logging

--- packages/aquarius/src/bot/commands/cat.js
+++ packages/aquarius/src/bot/commands/cat.js
@@ -1,7 +1,8 @@
 import debug from 'debug';
 import fetch from 'node-fetch';
+import getLogger from '../../core/logging/log';
 
-const log = debug('Cat');
+const log = getLogger('Cat');
 
 /** @type {import('../../typedefs').CommandInfo} */
 export const info = {
@@ -12,7 +13,7 @@ export const info = {
 
 export default async ({ aquarius }) => {
   aquarius.onCommand(/^cat$/i, async (message) => {
-    log('Image request');
+    log.info('Cat image request');
 
     try {
       const response = await fetch('https://api.thecatapi.com/v1/images/search', {
@@ -22,10 +23,10 @@ export default async ({ aquarius }) => {
       });
       const json = await response.json();
 
-      log(json);
+      log.info(json);
       message.channel.send({ file: json[0].url });
     } catch (e) {
-      log(e);
+      log.error(e);
       message.channel.send("Sorry, I wasn't able to get an image!");
     }
   });
