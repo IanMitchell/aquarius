70fd13b742ab6a856445f4f7578cc765b55de766
Jack LaFond <6956351+jacc@users.noreply.github.com>
2021-08-07 18:52:35 -0400

    Create eval.js

---
+++ packages/aquarius/src/bot/commands/eval.js
@@ -0,0 +1,22 @@
+import { randomValue } from '../../core/helpers/lists';
+import getLogger, { getMessageMeta } from '../../core/logging/log';
+
+const log = getLogger('eval');
+
+/** @type {import('../../typedefs').CommandInfo} */
+export const info = {
+  name: 'eval',
+  description: 'Evaluate arbitrary code.',
+  usage: '```@Aquarius eval```',
+};
+
+
+
+/** @type {import('../../typedefs').Command} */
+export default async ({ aquarius, analytics }) => {
+  aquarius.onCommand(/^eval .+$/i, (message) => {
+    message.channel.send(`https://cdn.discordapp.com/attachments/852975798255484928/873682246287114281/eval_these_nuts.mp4`);
+
+    analytics.trackUsage('eval', message);
+  });
+};
