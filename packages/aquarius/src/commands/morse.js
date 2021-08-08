66b8db2515f0ce70789ff232031915d2a7f6cfcf
Ian Mitchell <ian.mitchell@hey.com>
2021-08-07 18:59:16 -0700

    Migrate Morse

---
+++ packages/aquarius/src/commands/morse.js
@@ -0,0 +1,62 @@
+import {
+  SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+} from "@discordjs/builders";
+import morse from "morse";
+import { handleDeprecatedCommand } from "../core/commands/slash";
+import getLogger, { getInteractionMeta } from "../core/logging/log";
+
+const log = getLogger("Morse");
+
+const command = new SlashCommandBuilder()
+  .setName("morse")
+  .setDescription("Encode or decode morse code.");
+
+const commandEncode = new SlashCommandSubcommandBuilder()
+  .setName("encode")
+  .setDescription("Encodes a morse sequence")
+  .addStringOption((input) =>
+    input.setName("input").setDescription("Input string").setRequired(true)
+  );
+
+const commandDecode = new SlashCommandSubcommandBuilder()
+  .setName("decode")
+  .setDescription("Decodes a morse sequence")
+  .addStringOption((input) =>
+    input.setName("morse").setDescription("Morse sequence").setRequired(true)
+  );
+
+/** @type {import('../typedefs').Command} */
+export default async ({ aquarius, analytics }) => {
+  aquarius.onSlash([command, commandEncode], (interaction) => {
+    const input = interaction.options.getString("input");
+
+    log.info(`Encoding "${input}"`, getInteractionMeta(interaction));
+    interaction.reply(morse.encode(input));
+    analytics.trackInteraction("enocde", interaction);
+  });
+
+  aquarius.onSlash([command, commandDecode], (interaction) => {
+    const input = interaction.options.getString("sequence");
+
+    log.info(`Decoding "${input}"`, getInteractionMeta(interaction));
+    interaction.reply(morse.decode(input));
+    analytics.trackInteraction("decode", interaction);
+  });
+
+  /**
+   * @deprecated
+   */
+  aquarius.onCommand(
+    /^morse encode (?<input>.+)$/i,
+    handleDeprecatedCommand("morse-encode", analytics)
+  );
+
+  /**
+   * @deprecated
+   */
+  aquarius.onCommand(
+    /^morse decode (?<input>.+)$/i,
+    handleDeprecatedCommand("morse-decode", analytics)
+  );
+};
